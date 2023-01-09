2afea5743634cd2d740a335b0499740d61ad583e
EL Gabo <harcot.sys@gmail.com>
2023-01-08 19:14:14 -0500

    fin de los casos de negocio 20 disctint

--- 20distinct.sql
+++ 20distinct.sql
@@ -46,7 +46,7 @@ WHERE nationality NOT IN('RUS','AUT')
 GROUP BY nationality
 ORDER BY c_authors DESC, nationality ASC;
 
-/*desviación estándar STDDEV es una de las funciones muy cercanas al procesador ed la base de datos*/
+/*desviación estándar STDDEV es una de las funciones muy cercanas al procesador de la base de datos*/
 
 SELECT title, price FROM books ORDER BY price DESC limit 10;
 
@@ -62,3 +62,42 @@ JOIN authors AS a
     ON a.author_id = b.author_id
 GROUP BY nationality
 ORDER BY libros DESC;
+
+/*La gracia de agrupar es saber sobre que lo vpopy a agrupar*/
+
+/**Cuál es el precio MAX/MIN de un libro*/
+
+SELECT a.nationality, MAX(price), MIN(price)
+FROM books as b
+JOIN authors AS a
+    ON a.author_id = b.author_id
+GROUP BY nationality
+
+/*REPORTE FINAL DE PRÉSTAMOS*/
+
+SELECT c.name, t.type, b.title, a.name, a.nationality
+FROM transactions AS t
+LEFT JOIN clients AS c
+    ON c.client_id = t.client_id
+LEFT JOIN books as b
+    ON b.book_id = t.book_id
+LEFT JOIN authors AS a
+    ON b.author_id = a.author_id
+
+/*concatenar
+y trabajar con fechas
+SELECT TO_DAYS(NOW()) = Cuantos sias han pasado desde la fecha 0 hasta hoy;
+*/
+SELECT c.name, t.type, b.title,
+    CONCAT(a.name, " (", a.nationality,")") AS autor,
+    TO_DAYS(NOW()) - TO_DAYS(t.created_at) AS ago
+FROM transactions AS t
+LEFT JOIN clients AS c
+    ON c.client_id = t.client_id
+LEFT JOIN books as b
+    ON b.book_id = t.book_id
+LEFT JOIN authors AS a
+    ON b.author_id = a.author_id
+
+/*Cuantos d[ias han pasado desde el nacimiento de los clientes*/
+SELECT name, TO_DAYS(birthdate) FROM clients
